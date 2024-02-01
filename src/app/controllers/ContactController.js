12e8af14b4a97dcb349678960fb2a2096e40f1dd
Victor Hugo <torugoestudos@gmail.com>
2024-01-31 23:53:17 -0300

    first commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,47 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+    const contact = await ContactsRepository.findById(id);
+
+    if(!contact) {
+
+      return response.status(400).json( { error: 'User not found'})
+    }
+
+      response.json(contact);
+
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if(!contact) {
+
+      return response.status(400).json( { error: 'User not found'})
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204)
+  }
+}
+
+module.exports = new ContactController();
---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,43 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Victor',
+    email: 'torugoestudos@gmail.com',
+    phone: '2196926154',
+    category_id: v4(),
+
+  },
+  {
+    id: v4(),
+    name: 'Rhaysa',
+    email: 'rhaysa@gmail.com',
+    phone: '2196926154',
+    category_id: v4(),
+
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts)
+    })
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)
+    ))
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id)
+      resolve();
+    })
+
+  }
+}
+
+module.exports = new ContactsRepository();
